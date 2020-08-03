4fd23782d50e28736c16c9fda6c2e58ff6c323f8
lenkim <joenggyu0@gmail.com>
2020-08-03 22:08:36 +0900

    implement TodoHeader & TodoInput

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>TODO IT</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+;
+}
+</style>
